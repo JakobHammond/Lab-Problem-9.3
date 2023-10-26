8fd8b871a81ad59610b20d683ac60e4ca60c1f7f
JakobHammond <143117911+JakobHammond@users.noreply.github.com>
2023-10-26 13:15:55 -0400

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -9,11 +9,13 @@
 using namespace std;
 
 void displayRun(int values[], int size);
+bool hasRun(int values[], int size);
 int main(void){
     int values[20] = {1, 2, 5, 5, 3, 1, 2, 4, 3, 2, 2, 2, 2, 3, 6, 5, 5, 6, 3, 1};
     int size = 20;
     displayRun(values, size);
-    cout << endl;
+    cout << endl << hasRun(values, size) << endl;
+    
 }
 
 void displayRun(int values[], int size){
@@ -34,4 +36,12 @@ void displayRun(int values[], int size){
         }
         cout << " ";
     }
+}
+
+bool hasRun(int values[], int size){
+    bool adjacentVal = false;
+        for(int i = 0; i < size; i++){
+            if(values[i] == values[i + 1]) adjacentVal = true;
+        }
+        return adjacentVal;
 }
