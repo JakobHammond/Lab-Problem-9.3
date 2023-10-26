03ecaaebda5a98903023ca849f8f11c135b0ea4a
JakobHammond <143117911+JakobHammond@users.noreply.github.com>
2023-10-26 13:11:56 -0400

    Completed displayRun

--- main.cpp
+++ main.cpp
@@ -8,6 +8,30 @@
 #include <iostream>
 using namespace std;
 
+void displayRun(int values[], int size);
 int main(void){
-    
+    int values[20] = {1, 2, 5, 5, 3, 1, 2, 4, 3, 2, 2, 2, 2, 3, 6, 5, 5, 6, 3, 1};
+    int size = 20;
+    displayRun(values, size);
+    cout << endl;
+}
+
+void displayRun(int values[], int size){
+    bool inRun = false;
+    for(int i = 0; i < size; i++){
+        if(values[i] == values[i + 1]){
+            if(inRun == false){
+                cout << "(";
+                inRun = true;
+            }
+        }
+        cout << values[i];
+        if(values[i] != values[i + 1]){
+            if(inRun){
+                cout << ")";
+                inRun = false;
+            }
+        }
+        cout << " ";
+    }
 }
